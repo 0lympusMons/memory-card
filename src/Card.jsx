3251f8f0cf498bd0cb54d1693fa79ea97c8aec60
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2024-06-13 00:03:25 -0600

    save

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,44 +2,20 @@ import cardBack from "../public/card-back.png";
 import Tilt from "react-parallax-tilt";
 export default function Card({ card, flip, handleClick }) {
   const cardImg = <img width={"100px"} src={card.image} />;
+  const cardBG = <img width={"100px"} src="/card-back.png" alt="" />;
 
   return (
-    <Tilt
-      tiltReverse
-      reset
-      glareEnable={card.shiny || true}
-      glareMaxOpacity={0.4}
-      glareColor={card.shiny ? "#f1b818" : "#fff"}
-      glarePosition="all"
-      className={`card-container ${flip ? "back" : "front"}`}
-    >
+    <Tilt className={`card-container ${flip ? "hideCards" : "showCards"}`}>
       <div className="card-inner">
-        <div className="card-front">
-          <button
-            className="card"
-            data-shiny={card.shiny}
-            onClick={() => handleClick(card.code)}
-          >
-            {card.shiny && <div className="shiny-symbol" />}
-
-            <img
-              src={card.image}
-              alt={card.name}
-              className="card-image"
-              draggable="false"
-            />
-            <p className="card-name">
-              <span className="name">{card.name}</span>
-            </p>
-          </button>
+        <div
+          className="card-front card"
+          key={card.code}
+          onClick={() => handleClick(card.code)}
+        >
+          {cardImg}
         </div>
-        <div className="card-back">
-          <img
-            width={"100px"}
-            src={"/card-back.png"}
-            alt="pokemon card back"
-            className="back"
-          />
+        <div className="card-back card" key={card.code}>
+          {cardBG}
         </div>
       </div>
     </Tilt>
