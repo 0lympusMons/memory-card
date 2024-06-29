80d7753d58f11eaa7d1f71c5d80403d4051dbadc
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2024-06-29 14:38:57 -0600

    Scrap Framer Motion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
-import { animate, motion, stagger, useAnimate } from "framer-motion";
 export function Card({ card, flip, handleClick, index }) {
   const [loaded, setLoaded] = React.useState(false);
 
@@ -56,40 +55,5 @@ export function CardElements({
         />
       ));
 
-  const motionCards = cardsEls.map((card, index) => {
-    return (
-      <motion.div className="motion-card-container" key={index}>
-        {card}
-      </motion.div>
-    );
-  });
-
-  const [scope, animate] = useAnimate();
-  const allCards = Array.from(document.querySelectorAll(".card-container"));
-  const rotateAllCards = allCards.map((card, index) => {
-    const zIndex = [0, 1, 5, 2, 3];
-    return [
-      `#card-${index}`,
-      { rotate: index * 5 - 10, originY: 100 },
-      { duration: 0.1 },
-    ];
-  });
-  if (animateStart) {
-    // eslint-disable-next-line no-inner-declarations
-    async function animation() {
-      await animate(".motion-card-container", { position: "absolute" });
-      await animate(scope.current, { y: ["100vh", "50vh"] }, { duration: 1 });
-      await animate(rotateAllCards);
-
-      console.log(rotateAllCards);
-    }
-
-    animation();
-  }
-
-  return (
-    <motion.div ref={scope} className="cards-wrapper">
-      {motionCards}
-    </motion.div>
-  );
+  return <div className="cards-wrapper">{cardsEls}</div>;
 }
