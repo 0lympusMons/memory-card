bba4d9afd3dcba5ea647e5d31cba158a6cee99b5
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2024-07-02 00:37:05 -0600

    add firestore api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,16 @@
+import { firestore, userCollection } from "./firebase";
+import {
+  getDocs,
+  setDoc,
+  where,
+  query,
+  documentId,
+  getDoc,
+  collection,
+  addDoc,
+} from "firebase/firestore";
+userCollection;
+
 const DECK_API_BASE = "https://www.deckofcardsapi.com/api/deck";
 
 async function fetchJson(url) {
@@ -22,8 +35,47 @@ export async function drawCards(deckID) {
   try {
     const url = `${DECK_API_BASE}/${deckID}/draw/?count=10`;
     const data = await fetchJson(url);
-    return data.cards;
+    return Promise.resolve(data.cards);
   } catch (error) {
-    console.error("Error drawing cards:", error);
+    return Promise.reject("Error drawing cards:", error);
   }
+}
+
+export async function fetchUsers() {
+  try {
+    const querySnapshot = await getDocs(userCollection);
+    const users = [];
+    querySnapshot.forEach((doc) => {
+      users.push({ id: doc.id, ...doc.data() });
+    });
+    console.log(users.sort((a, b) => b.highscore - a.highscore));
+    return users;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getHighScore(id) {
+  const q = query(
+    collection(firestore, "users"),
+    //todo use $id
+    where(documentId(), "==", "1")
+  );
+
+  let highscore;
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data());
+    highscore = doc.data().highscore;
+    return doc.data();
+  });
+
+  return highscore;
+}
+export async function setHighScore(id, score) {
+  const highscore = await getHighScore();
+
+  if (score > highscore)
+    //todo use $id
+    await addDoc(userCollection, { username: "test", highscore: 19 });
 }
